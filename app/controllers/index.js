b042c623880baf6cfec1a01710518357138c91fe
RonnSanji <untrue.hu@gmail.com>
2015-02-02 12:42:23 +0800

    First commit

---
+++ app/controllers/index.js
@@ -0,0 +1,60 @@
+
+//===========================================================================
+// PROPERTIES
+//===========================================================================
+var navigation = Alloy.Globals.navigation = Alloy.createController("navigation");
+
+//===========================================================================
+// END OF PROPERTIES
+//===========================================================================
+/* -- Bootstrap your application below this line -- */
+//===========================================================================
+// FUNCTIONS
+//===========================================================================
+
+var init = function()
+{
+// App configuration
+	var conf = {
+		index: "mainPage",
+		defaultOpenTransition: {transition: 'slideInFromRight', duration: 150},
+		defaultBackTransition: {transition: 'slideInFromLeft', duration: 150},
+		indexOptions: {
+			topLevel: true,
+			viewMode: 'nav',
+			title: '',
+			identifier: 'index',
+		},
+		historyLimit: 10,
+		confirmOnExit: true,
+	};
+	navigation.init(conf);
+	// configuartion for distribution build
+	if(Alloy.CFG.APPLICATION_BUILD == Alloy.CFG.BUILD.DISTRIBUTION)
+	{
+
+
+	
+	}
+	else
+	{
+
+	}
+	
+};
+
+
+
+
+
+
+//===========================================================================
+// END OF FUNCTIONS
+//===========================================================================	
+//===========================================================================
+// LOGICS
+//===========================================================================
+init();
+//===========================================================================
+// END OF LOGICS
+//===========================================================================
